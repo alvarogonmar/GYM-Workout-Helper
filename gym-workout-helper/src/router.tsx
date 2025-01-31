52251936588084f7c7362fdd95f3cba4f17f5bd5
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-31 16:40:24 -0600

    imported layout

--- gym-workout-helper/src/router.tsx
+++ gym-workout-helper/src/router.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import IndexPage from "./views/IndexPage";
 import ExercisePage from "./views/ExercisePage";
+import Layout from "./layouts/Layout";
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<IndexPage />} />
-        <Route path="/favorites" element={<ExercisePage />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<IndexPage />} />
+          <Route path="/favorites" element={<ExercisePage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
