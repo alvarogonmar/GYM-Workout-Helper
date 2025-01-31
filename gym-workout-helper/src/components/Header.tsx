d082114954efc8d0774f7a8cb6163b93d3589760
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-31 16:25:33 -0600

    header updated

--- gym-workout-helper/src/components/Header.tsx
+++ gym-workout-helper/src/components/Header.tsx
@@ -1,3 +1,12 @@
 export default function Header() {
-  return <header></header>;
+  return (
+    <header className="bg-slate-800">
+      <div className="mx-auto container px-5 py-16">
+        <div className="flex justify-between items-center">
+          <div></div>
+          <nav></nav>
+        </div>
+      </div>
+    </header>
+  );
 }
