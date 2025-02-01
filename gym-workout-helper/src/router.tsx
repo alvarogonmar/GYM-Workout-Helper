e4f27b75ed6401653155a20213bd6a7de79d8ecd
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-31 19:57:34 -0600

    added header.css, buttons with animations, views search and user

--- gym-workout-helper/src/router.tsx
+++ gym-workout-helper/src/router.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import IndexPage from "./views/IndexPage";
 import ExercisePage from "./views/ExercisePage";
 import Layout from "./layouts/Layout";
+import SearchPage from "./views/SearchPage";
+import UserAccountPage from "./views/UserAccountPage";
 
 export default function AppRouter() {
   return (
@@ -10,6 +12,8 @@ export default function AppRouter() {
         <Route element={<Layout />}>
           <Route path="/" element={<IndexPage />} index />
           <Route path="/myexercises" element={<ExercisePage />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/user" element={<UserAccountPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
