13324584a9e9934cd1ee27c5e94e10912618d86f
alvarogonmar <alvarogonmar0@gmail.com>
2025-02-02 22:08:29 -0600

    statecreator added

--- gym-workout-helper/src/stores/exerciseSlice.ts
+++ gym-workout-helper/src/stores/exerciseSlice.ts
@@ -1,9 +1,11 @@
+import { StateCreator } from "zustand"
+
 type Muscle = {}
 
 export type ExerciseSliceType = {
     muscle: Muscle[]
 }
 
-export const createExerciseSlice = () => ({
+export const createExerciseSlice : StateCreator<ExerciseSliceType> = () => ({
     muscle: []
 })
