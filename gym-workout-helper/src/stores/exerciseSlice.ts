e078e73a3fe345d4436f61d6f09cf3f1a61fa7bd
alvarogonmar <alvarogonmar0@gmail.com>
2025-02-02 22:07:47 -0600

    type added

--- gym-workout-helper/src/stores/exerciseSlice.ts
+++ gym-workout-helper/src/stores/exerciseSlice.ts
@@ -1,4 +1,8 @@
+type Muscle = {}
 
+export type ExerciseSliceType = {
+    muscle: Muscle[]
+}
 
 export const createExerciseSlice = () => ({
     muscle: []
--- gym-workout-helper/src/stores/useAppStore.ts
+++ gym-workout-helper/src/stores/useAppStore.ts
@@ -1,6 +1,6 @@
 import { create } from "zustand";
 import { createExerciseSlice } from "./exerciseSlice";
 
-export const useAppStore = create( () => ({
-    ...createExerciseSlice() // tomar una copia
+export const useAppStore = create( (...a) => ({ //...a para tener set, get, api
+    ...createExerciseSlice(...a) // tomar una copia
 }))
