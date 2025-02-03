3b35779c57e059023a37f27a59a9ba98f8a2e2b2
alvarogonmar <alvarogonmar0@gmail.com>
2025-02-02 22:09:57 -0600

    imported useappstore

--- gym-workout-helper/src/views/ExercisePage.tsx
+++ gym-workout-helper/src/views/ExercisePage.tsx
@@ -1,4 +1,7 @@
+import { useAppStore } from "../stores/useAppStore";
+
 export default function ExercisePage() {
+  useAppStore((state) => state.muscle);
   return (
     <>
       <h1>My Exercises</h1>
