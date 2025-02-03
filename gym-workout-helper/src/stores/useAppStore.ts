a6e5c07562e9e25b3a3b584b87deb7f8c6cd493f
alvarogonmar <alvarogonmar0@gmail.com>
2025-02-02 22:04:24 -0600

    function called

--- gym-workout-helper/src/stores/useAppStore.ts
+++ gym-workout-helper/src/stores/useAppStore.ts
@@ -2,5 +2,5 @@ import { create } from "zustand";
 import { createExerciseSlice } from "./exerciseSlice";
 
 export const useAppStore = create( () => ({
-
+    ...createExerciseSlice() // tomar una copia
 }))
